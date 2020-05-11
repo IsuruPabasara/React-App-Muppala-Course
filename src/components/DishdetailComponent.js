fe7db03b9c39ccb34a0c62a4a992877a3604557e
Isuru Rajapakshe <rmisur@gmail.com>
2020-05-11 09:58:20 +0530

    route part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
-
+import {Card, CardImg, CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
     function RenderDish({dish}){
         return(
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>    
-                </CardBody>      
-            </Card>
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>    
+                    </CardBody>      
+                </Card>
+            </div>
         );
     }
 
@@ -17,17 +19,19 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
         if(allComments!=null){
             const comments = allComments.map((comment)=>{
                 return(
-                    <ul className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li>--{comment.author}, 
-                        &nbsp;
-                        {new Intl.DateTimeFormat('en-US', {
-                                year: 'numeric',
-                                month: 'short',
-                                day: '2-digit'
-                        }).format(new Date(Date.parse(comment.date)))}
-                        </li>     
-                    </ul>
+                    <div className="col-12 col-md-5 m-1">
+                        <ul className="list-unstyled">
+                            <li>{comment.comment}</li>
+                            <li>--{comment.author}, 
+                            &nbsp;
+                            {new Intl.DateTimeFormat('en-US', {
+                                    year: 'numeric',
+                                    month: 'short',
+                                    day: '2-digit'
+                            }).format(new Date(Date.parse(comment.date)))}
+                            </li>     
+                        </ul>
+                    </div>
                 )
             });
 
@@ -50,12 +54,19 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
             return(
                 <div className="container">
                     <div className="row">
-                        <div className="col-12 col-md-5 m-1">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to = '/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className='col-12'>
+                            <h3>{props.dish.name}</h3>
+                            <hr/>
+                        </div>
+                    </div>
+               
+                    <div className="row">
                             <RenderDish dish={props.dish}/>
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments allComments = {props.dish.comments}/>
-                        </div>
+                            <RenderComments allComments = {props.comments}/>
                     </div>
                 </div>
             );
